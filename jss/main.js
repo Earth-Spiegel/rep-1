6e301e50809c844398258cbe36378cd076f709e1
Meth Parig <metparig@gmail.com>
2022-11-16 19:13:30 -0700

    added js, modified css and index

---
+++ jss/main.js
@@ -0,0 +1,18 @@
+/* Smooth Scrolling */
+$('.dropdown-content a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      900,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
